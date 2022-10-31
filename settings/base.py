4019f9d7781cae79eb77ce4d56177323122bdc5c
Alexs18 <mclexis18@gmail.com>
2022-10-31 22:43:31 +0100

    cambios en la estructura del proyetco

--- settings/base.py
+++ settings/base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applications.login',
+    'applications.users'
 ]
 
 MIDDLEWARE = [
